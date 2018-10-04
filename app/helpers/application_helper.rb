8dadb95c110d649dad74380a1b0b811099327d3f
duongtnhat <duongtnhat@gmail.com>
2018-10-04 16:34:49 +0700

    gop commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def full_title(page_title = "")
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title page_title = ""
+    base_title = I18n.t "base_title"
     return base_title if page_title.empty?
     page_title + " | " + base_title
   end
