fbc666b16605dec2f670bf824d0a0dea998fff00
duongtnhat <duongtnhat@gmail.com>
2018-10-10 14:03:28 +0700

    chapter11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      sign_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        sign_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t "mail.inactivated_warning"
+        redirect_to root_url
+      end
     else
       flash[:danger] = t "signin.incorrect_signin"
       redirect_to signin_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  scope :show_user, -> {select :id, :name, :email}
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
   before_save {email.downcase!}
   validates :name,  presence: true,
     length: {maximum: Settings.constant.username_max_length}
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.constant.password_min_length},
     allow_nil: true
+  scope :show_user, -> {where(activated: true).select :id, :name, :email}
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ?
@@ -29,12 +30,29 @@ class User < ApplicationRecord
     update remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update remember_digest: nil
+  end
+
+  def activate
+    update activated: true
+    update activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
